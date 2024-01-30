b87facfc3c6769a7b42ada585c3425ad102e0f73
Theneillsaaco <157244812+Theneillsaaco@users.noreply.github.com>
2024-01-30 17:04:59 -0400

    Add files via upload

---
+++ Itlas/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿double perimetro = 0;
+String input = string.Empty;
+double lado = 0;
+
+//Perdirle al usuario el valor de las variables
+Console.WriteLine("Ingrese la longuitud del lado del cuadrado: ");
+input = Console.ReadLine();
+if (double .TryParse(input, out lado))
+{
+    perimetro = CalcularPerimetroCuadrado(lado);
+    Console.WriteLine($"El perimetro del cuadrado es: {perimetro}");
+}
+else
+{
+    Console.WriteLine("entrada no valida. POr favor, ingrese un numero valido.");
+}
+
+static double CalcularPerimetroCuadrado(double lado)
+{
+    return 4 * lado;
+}

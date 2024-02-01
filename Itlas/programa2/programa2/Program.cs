c2ec95894dd8f354d9c9cfb4f2d55b202e12fefc
Theneillsaaco <157244812+Theneillsaaco@users.noreply.github.com>
2024-02-01 18:59:15 -0400

    programa 2

---
+++ Itlas/programa2/programa2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace program2;
+public static class program
+{
+    static void Main()
+    {
+        // Declaración de variables
+        int num1, num2, num3, num4;
+        int suma;
+        int producto;
+
+        // Ingreso de datos
+        Console.WriteLine("Ingrese el primer número:");
+        num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo número:");
+        num2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el tercer número:");
+        num3 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el cuarto número:");
+        num4 = int.Parse(Console.ReadLine());
+
+        // Cálculo de la suma
+        suma = num1 + num2;
+
+        // Cálculo del producto
+        producto = num3 * num4;
+
+        // Salida de resultados
+        Console.WriteLine("La suma de los dos primeros números es: {0}", suma);
+        Console.WriteLine("El producto del tercero y el cuarto número es: {0}", producto);
+    }
+}

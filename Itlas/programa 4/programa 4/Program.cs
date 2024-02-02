ebdf222c08412f052a58c4cb1c70e4e000d1acda
Theneillsaaco <157244812+Theneillsaaco@users.noreply.github.com>
2024-02-01 20:07:43 -0400

    programa 4

---
+++ Itlas/programa 4/programa 4/Program.cs
@@ -0,0 +1,51 @@
+﻿
+
+namespace programa4
+{
+    public static class program
+    {
+        public static void Main()
+        {
+            //declaracion de variables
+            double  num1, num2, suma, diferencia, producto, divicion;
+
+            //perdile al usuario las variables
+            Console.WriteLine("Ingrese el primer numero: ");
+            num1 = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo numero: ");
+            num2 = Double.Parse(Console.ReadLine());
+
+            //verificacion de las variables
+            if (num1 > num2)
+            {
+                suma = num1 + num2;
+                diferencia = num1 - num2;
+
+                //mostrar resutados
+                Console.WriteLine($"la suma es: {suma}");
+                Console.WriteLine($"La diferencia es: {diferencia}");
+            }
+            else
+            {
+                //si el segundo es mayor o igual
+                if (num2 != 0)
+                {
+
+                    producto = num1 * num2;
+                    divicion = num1 / num2;
+
+                    //mostrar resutados
+                    Console.WriteLine($"El producto es: {producto}");
+                    Console.WriteLine($"La divicion es: {divicion}");
+                }
+                else
+                {
+                    Console.WriteLine("Su numero es invalido o No es posible dividir por cero.");
+                }
+            }
+            
+            
+        }
+    }
+}
